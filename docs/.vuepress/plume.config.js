9111512f387acfc23347276e1eb42b06cbcddaa9
mcenahle <i@mn.edu.kg>
2026-04-01 15:07:36 +0800

    新增绘制的图标，并在配置文件中引用图标

--- docs/.vuepress/plume.config.js
+++ docs/.vuepress/plume.config.js
@@ -19,15 +19,15 @@ import { defineCollection } from 'vuepress-theme-plume'
  * @see https://theme-plume.vuejs.press/config/theme/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: '/images/logo.png',
 
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'github', link: 'https://github.com/mcenahle/' },
   ],
-  // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
-  // aside: true, // 页内侧边栏， 默认显示在右侧
+  navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
+  aside: true, // 页内侧边栏， 默认显示在右侧
   outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
   /**
